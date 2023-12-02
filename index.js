797c4e8aa3a372f5c5e908493f52c24fea533e11
piasahmed1 <ahmedpias229@gmail.com>
2023-12-02 15:11:19 +0600

    added some new

--- index.js
+++ index.js
@@ -252,41 +252,6 @@ async function run() {
 
     })
 
-    //middlewares
-
-    // const verifyToken = (req, res, next) => {
-    //   // console.log(req.headers.authorization);
-    //   if (!req.headers.authorization) {
-    //     return res.status(401).send({ message: 'unauthorised access' });
-    //   }
-
-    //   const token = req.headers.authorization.split(' ')[1];
-    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    //     if (err) {
-    //       return res.status(401).send({ message: 'unauthorised access' })
-    //     }
-    //     req.decoded = decoded;
-    //     next();
-    //   })
-
-    // }
-
-    //use verify admin after verifytoken
-    //  const verifyAdmin = async (req, res, next) => {
-
-    //   const email = req.decoded.email;
-    //   const query = { email: email };
-    //   const user = await userscollection.findOne(query);
-    //   const isAdmin = user?.role === 'admin';
-    //   if (!isAdmin) {
-    //     return res.status(403).send({ message: 'forbidden access' })
-    //   }
-    //   next();
-    // }
-
-
-
-
 
     //user releted api
 
